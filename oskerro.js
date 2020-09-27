e9c84bb9cf40b477212a9634bc9e90ed36d121da
Oscar Asikoyo <64526213+95asikoyo@users.noreply.github.com>
2020-09-27 16:53:35 +0300

    Update oskerro.js

--- oskerro.js
+++ oskerro.js
@@ -41,6 +41,19 @@ sr.reveal('.animate-bottom', {
     delay: 600
 });
 
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+      $("#topBtn").fadeIn();
+    } else {
+      $("#topBtn").fadeOut();
+    }
+  });
+
+  $("#topBtn").click(function () {
+    $("html ,body").animate({ scrollTop: 0 }, 1200);
+  });
+});
 
 
 
